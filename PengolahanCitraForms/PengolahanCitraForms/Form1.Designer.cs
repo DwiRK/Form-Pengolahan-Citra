db19608468e87c8e7fb72cbbfd614fca88ee98a8
unknown <dwirizkykurniawan98@gmail.com>
2019-02-20 14:20:37 +0700

    Add save, open, exit in file menu, add view image and rgb toolbar

---
+++ PengolahanCitraForms/PengolahanCitraForms/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace PengolahanCitraForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.generateColorBtn = new System.Windows.Forms.Button();
+            this.mixPictureBox = new System.Windows.Forms.PictureBox();
+            this.blueValue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.greenValue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.redValue = new System.Windows.Forms.TextBox();
+            this.greenPictureBox = new System.Windows.Forms.PictureBox();
+            this.bluePictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.redPictureBox = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mixPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bluePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveToolStripMenuItem.Text = "Save As";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 424);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 398);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "View Image";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(170, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(419, 389);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.generateColorBtn);
+            this.tabPage2.Controls.Add(this.mixPictureBox);
+            this.tabPage2.Controls.Add(this.blueValue);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.greenValue);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.redValue);
+            this.tabPage2.Controls.Add(this.greenPictureBox);
+            this.tabPage2.Controls.Add(this.bluePictureBox);
+            this.tabPage2.Controls.Add(this.pictureBox3);
+            this.tabPage2.Controls.Add(this.redPictureBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 398);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "RGB";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(479, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Mix";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(339, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Blue";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(196, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Green";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Red";
+            // 
+            // generateColorBtn
+            // 
+            this.generateColorBtn.Location = new System.Drawing.Point(11, 306);
+            this.generateColorBtn.Name = "generateColorBtn";
+            this.generateColorBtn.Size = new System.Drawing.Size(149, 26);
+            this.generateColorBtn.TabIndex = 11;
+            this.generateColorBtn.Text = "Generate Color";
+            this.generateColorBtn.UseVisualStyleBackColor = true;
+            this.generateColorBtn.Click += new System.EventHandler(this.generateColorBtn_Click);
+            // 
+            // mixPictureBox
+            // 
+            this.mixPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mixPictureBox.Location = new System.Drawing.Point(422, 6);
+            this.mixPictureBox.Name = "mixPictureBox";
+            this.mixPictureBox.Size = new System.Drawing.Size(132, 120);
+            this.mixPictureBox.TabIndex = 10;
+            this.mixPictureBox.TabStop = false;
+            // 
+            // blueValue
+            // 
+            this.blueValue.Location = new System.Drawing.Point(60, 261);
+            this.blueValue.Name = "blueValue";
+            this.blueValue.Size = new System.Drawing.Size(100, 20);
+            this.blueValue.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Blue";
+            // 
+            // greenValue
+            // 
+            this.greenValue.Location = new System.Drawing.Point(60, 230);
+            this.greenValue.Name = "greenValue";
+            this.greenValue.Size = new System.Drawing.Size(100, 20);
+            this.greenValue.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Green";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Red";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // redValue
+            // 
+            this.redValue.Location = new System.Drawing.Point(60, 195);
+            this.redValue.Name = "redValue";
+            this.redValue.Size = new System.Drawing.Size(100, 20);
+            this.redValue.TabIndex = 4;
+            this.redValue.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // greenPictureBox
+            // 
+            this.greenPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.greenPictureBox.Location = new System.Drawing.Point(146, 6);
+            this.greenPictureBox.Name = "greenPictureBox";
+            this.greenPictureBox.Size = new System.Drawing.Size(132, 120);
+            this.greenPictureBox.TabIndex = 3;
+            this.greenPictureBox.TabStop = false;
+            this.greenPictureBox.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // bluePictureBox
+            // 
+            this.bluePictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bluePictureBox.Location = new System.Drawing.Point(284, 6);
+            this.bluePictureBox.Name = "bluePictureBox";
+            this.bluePictureBox.Size = new System.Drawing.Size(132, 120);
+            this.bluePictureBox.TabIndex = 2;
+            this.bluePictureBox.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(235, 195);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(132, 120);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // redPictureBox
+            // 
+            this.redPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.redPictureBox.Location = new System.Drawing.Point(8, 6);
+            this.redPictureBox.Name = "redPictureBox";
+            this.redPictureBox.Size = new System.Drawing.Size(132, 120);
+            this.redPictureBox.TabIndex = 0;
+            this.redPictureBox.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mixPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bluePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox redValue;
+        private System.Windows.Forms.PictureBox greenPictureBox;
+        private System.Windows.Forms.PictureBox bluePictureBox;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox redPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button generateColorBtn;
+        private System.Windows.Forms.PictureBox mixPictureBox;
+        private System.Windows.Forms.TextBox blueValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox greenValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ PengolahanCitraForms/PengolahanCitraForms/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PengolahanCitraForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            redWhiteBox();
+            rgbPictureBox(255, 255, 255, 200);
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Filter = "jpg (*.jpg)|*.jpg|bmp (*.bmp)|*.bmp|png (*.png)|*.png";
+
+            if (sfd.ShowDialog() == DialogResult.OK && sfd.FileName.Length > 0)
+            {
+                pictureBox1.Image.Save(sfd.FileName);
+            }
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "jpg (*.jpg)|*.jpg|bmp (*.bmp)|*.bmp|png (*.png)|*.png";
+
+            if (ofd.ShowDialog() == DialogResult.OK && ofd.FileName.Length > 0)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void redWhiteBox()
+        {
+            Bitmap bmp1 = new Bitmap(200, 140);
+
+            Color merah = Color.FromArgb(255, 0, 0);
+            Color putih = Color.FromArgb(255, 255, 255);
+
+            for (int y = 0; y < 70; y++)
+            {
+                for (int x = 0; x < 200; x++)
+                {
+                    bmp1.SetPixel(x, y, merah);
+                }
+            }
+
+            for (int y = 70; y < 140; y++)
+            {
+                for (int x = 0; x < 200; x++)
+                {
+                    bmp1.SetPixel(x, y, putih);
+                }
+            }
+
+            pictureBox3.Image = new Bitmap(bmp1.Height, bmp1.Width);
+            pictureBox3.Image = bmp1;
+        }
+
+        private void rgbPictureBox(int redColorValue, int greenColorValue, int blueColorValue, int pictureSize)
+        {
+            Bitmap bitRed = new Bitmap(pictureSize, pictureSize);
+            Bitmap bitGreen = new Bitmap(pictureSize, pictureSize);
+            Bitmap bitBlue = new Bitmap(pictureSize, pictureSize);
+
+            Color red = Color.FromArgb(redColorValue, 0, 0);
+            Color green = Color.FromArgb(0, greenColorValue, 0);
+            Color blue = Color.FromArgb(0, 0, blueColorValue);
+
+            for (int y = 0; y < pictureSize; y++)
+            {
+                for (int x = 0; x < pictureSize; x++)
+                {
+                    bitRed.SetPixel(x, y, red);
+                    bitGreen.SetPixel(x, y, green);
+                    bitBlue.SetPixel(x, y, blue);
+                }
+            }
+
+            redPictureBox.Image = new Bitmap(bitRed.Height, bitRed.Width);
+            redPictureBox.Image = bitRed;
+
+            greenPictureBox.Image = new Bitmap(bitGreen.Height, bitGreen.Width);
+            greenPictureBox.Image = bitGreen;
+
+            bluePictureBox.Image = new Bitmap(bitBlue.Height, bitBlue.Width);
+            bluePictureBox.Image = bitBlue;
+        }
+
+        private void generateColorBtn_Click(object sender, EventArgs e)
+        {
+            int newRedValue, newGreenValue, newBlueValue;            Int32.TryParse(redValue.Text, out newRedValue);            Int32.TryParse(greenValue.Text, out newGreenValue);            Int32.TryParse(blueValue.Text, out newBlueValue);
+
+            rgbPictureBox(newRedValue, newGreenValue, newBlueValue, 200);
+
+            Bitmap bitCampur = new Bitmap(200, 200);
+
+            Color mix = Color.FromArgb(newRedValue, newGreenValue, newBlueValue);
+
+            for (int y = 0; y < 200; y++)            {                for (int x = 0; x < 200; x++)                {                    bitCampur.SetPixel(x, y, mix);                }            }
+
+            mixPictureBox.Image = new Bitmap(bitCampur.Height, bitCampur.Width);            mixPictureBox.Image = bitCampur;
+        }
+    }
+}
